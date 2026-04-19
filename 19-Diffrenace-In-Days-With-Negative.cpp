6b20e5f07357079cef975a0198c747ebefe4a5f4
Pro_Mr_SiR <mgoihtahmumb@gmail.com>
2026-04-19 16:52:50 +0300

    Create 19-Diffrenace-In-Days-With-Negative.cpp

---
+++ 19-Diffrenace-In-Days-With-Negative.cpp
@@ -0,0 +1,157 @@
+#pragma warning(disable : 4996)
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+short ReadMonth()
+{
+    short Month;
+    cout << "\nPlease enter a Month? ";
+    cin >> Month;
+    return Month;
+}
+
+short ReadYear()
+{
+    short Year;
+    cout << "\nPlease enter a year? ";
+    cin >> Year;
+    return Year;
+}
+
+short ReadDay()
+{
+    short Day;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+    return Day;
+}
+
+struct stDate
+{
+    short Year;
+    short Month;
+    short Day;
+};
+
+bool isLeapYear(short Year)
+{
+    return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+
+short NumberOfDaysInAMonth(short Year, short Month)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    short NumberOfDays[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    return (Month == 2) ? (isLeapYear(Year) ? 29 : 28) : NumberOfDays[Month];
+}
+
+bool IsDate1BeforeDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year < Date2.Year) ? true : ((Date1.Year == Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month == Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+}
+
+bool IsLastDayInMonth(stDate Date)
+{
+    return (Date.Day == NumberOfDaysInAMonth(Date.Year, Date.Month));
+}
+
+bool IsLastMonthInYear(short Month)
+{
+    return (Month == 12);
+}
+
+stDate IncreaseDateByOneDay(stDate Date)
+{
+    if (IsLastDayInMonth(Date))
+    {
+        if (IsLastMonthInYear(Date.Month))
+        {
+            Date.Day = 1;
+            Date.Month = 1;
+            Date.Year++;
+        }
+        else
+        {
+            Date.Day = 1;
+            Date.Month++;
+        }
+    }
+    else
+    {
+        Date.Day++;
+    }
+
+    return Date;
+}
+
+void SwapDates(stDate& Date1, stDate& Date2)
+{
+    stDate TempDate;
+
+    TempDate.Year = Date1.Year;
+    TempDate.Month = Date1.Month;
+    TempDate.Day = Date1.Day;
+
+    Date1.Year = Date2.Year;
+    Date1.Month = Date2.Month;
+    Date1.Day = Date2.Day;
+
+    Date2.Year = TempDate.Year;
+    Date2.Month = TempDate.Month;
+    Date2.Day = TempDate.Day;
+}
+
+int GetDifferenceInDays(stDate Date1, stDate Date2, bool IncludeEndDay = false)
+{
+    int Days = 0;
+    short SwapFlagValue = 1;
+
+    if (!IsDate1BeforeDate2(Date1, Date2))
+    {
+        SwapDates(Date1, Date2);
+        SwapFlagValue = -1;
+    }
+
+    while (IsDate1BeforeDate2(Date1, Date2))
+    {
+        Days++;
+        Date1 = (IncreaseDateByOneDay(Date1));
+    }
+
+    return IncludeEndDay ? ++Days * SwapFlagValue : Days * SwapFlagValue;
+}
+
+stDate ReadFullDate()
+{
+    stDate Date;
+
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+
+    return Date;
+}
+
+
+
+
+int main()
+{
+    stDate Date1 = ReadFullDate();
+    stDate Date2 = ReadFullDate();
+
+    cout << "\nDiffrence is: "
+        << GetDifferenceInDays(Date1, Date2) << " Day(s).";
+
+    cout << "\nDiffrence (Including End Day) is: "
+        << GetDifferenceInDays(Date1, Date2, true) << " Day(s).";
+
+    return 0;
+}
